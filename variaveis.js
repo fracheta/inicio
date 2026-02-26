330fa4bcb5c82cae7e9fad588d3874c3f4f05e9c
fracheta <gabriel.fracheta.souza@escola.pr.gov.br>
2026-02-26 09:51:09 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,40 @@
+let a=40
+let b=10
+let c=20
+let total=0
+let todalde =0
+let x=5
+total=(a+b+c)
+totalde=(a+b+c)/x 
+console.log("o resultado de A+B+C",total );
+console.log("o resultado é de D",totalde);
+console.log(outraconta());
+console.log(media());
+
+// inicio da function
+function outraconta (){
+    var d =10
+    var e =20
+    var f =30
+    var tot =0
+    var totd=0
+    var xx=100
+    tot=(d+e+f)
+    totd=(d+e+f)/xx
+    //console.log("o resultado de D+E+F",tot);
+    //console.log("o resultado de D+E+F/xx",totd);
+    return tot
+    return totd
+}
+
+
+
+
+function media (){ 
+    n1=30
+    n2=90
+    n3=100
+    ine=(n1+n2+n3)/3
+    //console.log("o resultado da nota é =",ine.toFixed(1));
+    return ine.toFixed(1)
+}
