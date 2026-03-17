769fa7aa433095ee419ed8f4c5d250a6030e6fa2
fracheta <gabriel.fracheta.souza@escola.pr.gov.br>
2026-03-17 08:43:36 -0300

    Update filtrar.js

--- filtrar.js
+++ filtrar.js
@@ -1,14 +1,21 @@
-const usuarios = [
+const usuarios = [  //lista(array)
     {nome: "carlos", idade: 32},
     {nome:"Ana", idade: 28},
     {nome:"João", idade: 40}
 ]
 
+//encontrando um objeto - find == busca igual
 const ana = usuarios.filter(usuario => usuario.nome === "Ana")
 console.log(ana)
 
+//filtrar objetos
 const usuariosAcimaDe30 = usuarios.filter(usuario => usuario.idade > 30)
 console.log(usuariosAcimaDe30)
 
-const usuariosOrdenadosPorIdade = usuarios.sort((a, b) => a.idade - b.idade)
-console.log(usuariosOrdenadosPorIdade)
+//ordenando objetos por idade
+const usuariosOrdenadosPor = usuarios.sort((a, b) => a.idade - b.idade)
+console.log(usuariosOrdenadosPor)
+
+//ordenando objetos por idade
+const usuariosOrdenadosPorV = usuarios.sort((a, b) => b.idade - a.idade)
+console.log(usuariosOrdenadosPor)
